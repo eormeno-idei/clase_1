11ad8463b18c63572f522ff67eed00b957d35640
Emilio Gustavo Ormeno <eormeno@iinfo.unsj.edu.ar>
2025-08-13 22:48:23 +0000

    Añade funcionalidad para mostrar personas mayores de 20 años en práctica_1.php

--- práctica_1.php
+++ práctica_1.php
@@ -1,5 +1,27 @@
 <?php
 
 $variable = "Hola, mundo!";
+$mensaje = "Cruel";
 
-echo $variable . PHP_EOL;
+// echo $variable . $mensaje . PHP_EOL;
+// echo "El mensaje: $variable $mensaje" . PHP_EOL;
+
+// Arreglos simples
+// $arreglo = ["Hola", "mundo", "Cruel"];
+
+// for ($i = 0; $i < count($arreglo); $i++) {
+//     echo $arreglo[$i] . " ";
+// }
+// echo PHP_EOL;
+
+$persona[0] = ["apellido" => "Ormeño", "nombre" => "Emilio", "edad" => 30];
+$persona[1] = ["apellido" => "Scheffer", "nombre" => "Maru", "edad" => 15];
+$persona[2] = ["apellido" => "Gonzalez", "nombre" => "Juan", "edad" => 25];
+$persona[3] = ["apellido" => "Varela", "nombre" => "Martín", "edad" => 18];
+
+// muestre por consola las personas que tienen más de 20 años
+foreach ($persona as $p) {
+    if ($p["edad"] > 20) {
+        echo "Nombre: " . $p["nombre"] . ", Apellido: " . $p["apellido"] . ", Edad: " . $p["edad"] . PHP_EOL;
+    }
+}
