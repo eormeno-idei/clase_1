b65ccadc9c0da62db43815137dda5e5a3509ccdf
Rebollo Peña Juan Ignacio <jrebollo@iinfo.unsj.edu.ar>
2025-08-13 22:48:30 +0000

    Elimina el código comentado y ajusta la estructura del archivo práctica_1.php

--- práctica_1.php
+++ práctica_1.php
@@ -1,5 +1,16 @@
 <?php
 
-$variable = "Hola, mundo!";
+//$variable = "Hola, mundo!";
+//$mensaje = "adios";
+//echo $variable . PHP_EOL;
+//echo $mensaje . PHP_EOL;
 
-echo $variable . PHP_EOL;
+$arreglo =["hola","adios"]; 
+
+//foreach ($arreglo as $i){
+//   echo $i . PHP_EOL;
+//}
+
+for ($i=0; $i<count($arreglo); $i++){
+    echo $arreglo[$i] . " ; ". PHP_EOL;
+}
